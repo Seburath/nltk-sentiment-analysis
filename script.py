3be08d792865253a03d8d4f1ccc9f4cf0c5ba825
seburath <seburath@gmail.com>
2018-10-22 19:19:30 -0500

    iq

---
+++ script.py
@@ -0,0 +1,14 @@
+
+from nltk.corpus import wordnet
+
+antonyms = []
+
+for syn in wordnet.synsets("small"):
+
+    for l in syn.lemmas():
+
+        if l.antonyms():
+
+            antonyms.append(l.antonyms()[0].name())
+
+print(antonyms)
